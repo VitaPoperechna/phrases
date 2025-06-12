80e451226d1e13ee4e4d3ff46e40c9a6733c7d88
VitaPoperechna <vitapoperechna@gmal.com>
2025-06-12 05:45:21 +0200

    feat: added data in phrase class

--- src/main/java/com/femcoders/phrases/models/Phrase.java
+++ src/main/java/com/femcoders/phrases/models/Phrase.java
@@ -1,4 +1,40 @@
 package com.femcoders.phrases.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="phrases")
 public class Phrase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String frase;
+    private String author;
+
+    public Phrase() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public void setFrase(String frase) {
+        this.frase = frase;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
